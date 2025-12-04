c7c577cb90fe96be6f15d5c3c910cd19ca303b96
rezaul-karim <rezaul.coderpro@gmail.com>
2025-12-04 13:13:45 +0600

    update new categpry job commit

--- src/pages/roothome/JobCategory.jsx
+++ src/pages/roothome/JobCategory.jsx
@@ -214,7 +214,7 @@ const JobCategory = () => {
   ];
 
   return (
-    <div className="bg-linear-to-br from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-12">
+    <div className="bg-linear-to-br from-gray-50 to-gray-100 pb-12 pt-4 px-4 sm:px-6 lg:px-12">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 bg-white rounded-xl shadow-lg p-6">
